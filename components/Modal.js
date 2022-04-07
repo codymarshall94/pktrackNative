06292ee3f8c8c4f47713fd2c0e1f95fe7eca1dca
Cody Marshall <codymstuntsx@gmail.com>
2022-04-07 16:44:43 -0700

    added styling changes to the modal card

--- components/Modal.js
+++ components/Modal.js
@@ -9,24 +9,61 @@ import { StyleSheet } from "react-native";
 function SkillModal(props) {
 
     return(
-    <View>
         <Modal 
             isVisible={props.isModalVisible}
-            style={styles.modalStyle}
-            >
-            <View style={{flex: 1}}>
-                <Text>{props.activeSkill.name}</Text>
-                <Text>{props.activeSkill.description}</Text>
-                <Button title="Hide Modal" onPress={props.toggleModal}/>
+            style={styles.modalStyle}>
+            <View> 
+                <View style={styles.modalHeader}>
+                    <Text style={styles.modalHeaderTitle}>{props.activeSkill.name}</Text>
+                </View>
+                <View>
+                    <Text style={styles.modalSubHeader}>Description:</Text>
+                    <Text style={styles.modalContentText}>{props.activeSkill.description}</Text>
+                </View>
+                <View>
+                    <Text style={styles.modalSubHeader}>Prerequisites:</Text>
+                </View>
+                <View>
+                    <Text style={styles.modalSubHeader}>Subsquent:</Text>
+                </View>
+                <View style={{ width: 100, height: 100, alignSelf: 'center'}}>
+                    <Button 
+                        title="Go Back" 
+                        type='outline' 
+                        onPress={props.toggleModal}
+                        style={{width: 40, marginTop: 20}}/>
+                </View>
             </View>
         </Modal>
-    </View>
     )
 }
 
 const styles = StyleSheet.create({
     modalStyle: {
+        flex: 1,
+        justifyContent: 'space-between',
         backgroundColor: 'white',
+    },
+    modalHeader: {
+        height: 100,
+        backgroundColor: 'blue',
+        fontSize: 18,
+        justifyContent: "center",
+    },
+    modalHeaderTitle: {
+        fontSize: 18,
+        alignSelf: "center",
+        color: 'white',
+    },
+
+    modalSubHeader: {
+        alignSelf: 'center',
+        fontSize: 16
+    },
+
+    modalContentText: {
+        fontSize: 12,
     }
   });
+
 export default SkillModal;
